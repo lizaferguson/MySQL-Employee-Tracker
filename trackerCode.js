6ebcae0fa1282fcc4e025a512f8d9baed9fdc8ed
lizaferguson <lizaferguson19@gmail.com>
2021-04-28 15:52:05 -0400

    added gitignore and env

--- trackerCode.js
+++ trackerCode.js
@@ -2,11 +2,13 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Biggestloser!',
-    database: 'employee_trackerDB',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
